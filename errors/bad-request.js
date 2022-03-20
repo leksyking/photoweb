1e5073db13ab95c91997f148b05f9da82732e224
Leksyking <gbemilekeogundipe@gmail.com>
2022-03-20 05:45:08 -0700

    user model

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,7 +1,7 @@
-const customAPIError = require('./custom')
+const CustomAPIError = require('./custom')
 const {StatusCodes} = require('http-status-codes')
 
-class BadRequestError extends customAPIError{
+class BadRequestError extends CustomAPIError{
     constructor(message){
         super(message)
         this.statusCode = StatusCodes.BAD_REQUEST
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const {register, login, logout} = require('../controllers/authController')
 
-router.get('/register', register)
-router.get('/login', login)
-router.get('/logout', logout)
+router.post('/register', register)
+router.post('/login', login)
+router.post('/logout', logout)
 
 module.exports = router;
